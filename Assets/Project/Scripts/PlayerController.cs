13f0eaa943612fa34b32ec775fcbe4583fc2fc16
JooKissa <s2000958@edu.bc.fi>
2022-03-11 14:24:19 +0200

    basic controls

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -2,9 +2,38 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    private Vector2 jumpForce = new Vector2(0, 300);
+    private Rigidbody2D rb2d;
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(jumpForce);
+        }
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPosition.y > Screen.height || screenPosition.y < 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Die();
+    }
 }
